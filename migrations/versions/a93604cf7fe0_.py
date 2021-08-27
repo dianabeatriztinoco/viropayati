3f03d205148b38d9dc3c2b3fbda49e9722f01f75
Diana Beatriz Tinoco <dianabeatriztinoco@gmail.com>
2021-08-26 23:15:12 -0500

    migration

---
+++ migrations/versions/a93604cf7fe0_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: a93604cf7fe0
+Revises: 48dc86c71550
+Create Date: 2021-08-26 23:06:04.518729
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'a93604cf7fe0'
+down_revision = '48dc86c71550'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
