1eef14d7ff2af2bb3bce2ab49d3835572a37511e
Diana Beatriz Tinoco <dianabeatriztinoco@gmail.com>
2021-08-31 10:45:55 -0500

    changed revision number for migration file

--- migrations/versions/a93604cf7fe0.py
+++ migrations/versions/a93604cf7fe0.py
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '5b62c7048e92'
+revision = 'a93604cf7fe0'
 down_revision = None
 branch_labels = None
 depends_on = None
