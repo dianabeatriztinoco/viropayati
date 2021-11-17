64b99cbcb74c76f89cb28d79b69ba0a760cea5e7
Diana Beatriz Tinoco <dianabeatriztinoco@gmail.com>
2021-11-16 20:30:12 -0600

    d

---
+++ react-app/src/components/yogaClassForm/note.js
@@ -0,0 +1,64 @@
+const fizzBuzz = (x, y) => {
+    for(i = x, i <= y + 1, i++){
+        if(i % 3 == 0){
+            console.log("fizz")
+        }
+
+        if(i % 5 == 0 ){
+            console.log("buzz")
+        }
+
+    }
+}
+
+fizzBuzz(1, 500)
+
+
+module.exports = {
+    up: (queryInterface, Sequelize) => {
+        return queryInterface.createTable("Bagel_types", {
+            index: {
+                allowNull: false, 
+                autoIncrement: true, 
+                primaryKey: true, // automatically not allowed to be null. 
+                type: Sequelize.INTEGER
+            }, 
+
+            toppings: {
+                allowNull: false, 
+                type: Sequelize.VARCHAR
+            }, 
+
+            cost: {
+                allowNull: false, 
+                type: Sequelize.INTEGER
+            },
+
+            creation_date: {
+                allowNull: false, 
+                type: Sequelize.DATE
+            }
+        }
+    }
+}
+
+
+
+
+// PHP
+
+$text = "apples";
+
+<?php
+
+
+echo str_replace("p", "q", $text);
+
+?>
+
+
+
+const checkboxes = document.querySelectorAll('input[type="checkbox"]:unchecked')
+
+
+checkboxes.checked = true 
